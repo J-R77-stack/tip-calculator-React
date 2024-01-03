abc70793a6787365d42667bb444641ec430ec3b5
Johnny Ramsay <johnnyramsay@Johnnys-MacBook-Air.local>
2024-01-03 17:19:01 +0000

    start adding jsx components to app.js, thinking about logic and some styling

--- src/App.js
+++ src/App.js
@@ -1,1 +1,55 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="App">
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage> Select Tip %</SelectPercentage>
+      <NumberOfPeople />
+      <TipAmount />
+      <Total />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput() {
+  return (
+    <div>
+      <label>Bill</label>
+      <input type="number" placeholder="$                             0" />
+    </div>
+  );
+}
+
+function SelectPercentage({ children }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <button value="5">5%</button>
+      <button value="10">10%</button>
+      <button value="15">15%</button>
+      <button value="25">25%</button>
+      <button value="50">50%</button>
+      <button value="0">Custom</button>
+    </div>
+  );
+}
+
+function NumberOfPeople() {
+  return (
+    <div>
+      <label>Number of People</label>
+      <input type="number" placeholder="0" />
+    </div>
+  );
+}
+function TipAmount() {}
+function Total() {}
+function Reset() {}
