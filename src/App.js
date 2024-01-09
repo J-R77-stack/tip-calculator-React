380f73f75b820503af018b5d21abde8aa81c5860
Johnny Ramsay <johnnyramsay@Johnnys-MacBook-Air.local>
2024-01-09 17:52:34 +0000

    add logic for button percentages and custome button

--- src/App.js
+++ src/App.js
@@ -10,13 +10,25 @@ export default function App() {
 
 function TipCalculator() {
   const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [change, setChangeBill] = useState("");
+
+  const tip = (bill * percentage1) / 100;
+
+  const tip2 = (bill * change) / 100;
+
   return (
     <div>
       <BillInput bill={bill} onSetBill={setBill} />
-      <SelectPercentage />
+      <SelectPercentage
+        percentage={percentage1}
+        onClick={setPercentage1}
+        onChangeBill={setChangeBill}
+        change={change}
+      />
       <NumberOfPeople />
-      <TipAmount />
-      <Total bill={bill} />
+      <TipAmount tip={tip} tip2={tip2} />
+      <Total bill={bill} tip={tip} tip2={tip2} />
       <Reset />
     </div>
   );
@@ -36,20 +48,36 @@ function BillInput({ bill, onSetBill }) {
   );
 }
 
-function SelectPercentage() {
+function SelectPercentage({ onClick, change, onChangeBill }) {
   return (
     <>
       <div>
         <label className="selectTip">Select Tip %</label>
-        <button value="5">5%</button>
-        <button value="10">10%</button>
-        <button value="15">15%</button>
+        <button value="5" onClick={(e) => onClick(Number(e.target.value))}>
+          5%
+        </button>
+        <button value="10" onClick={(e) => onClick(Number(e.target.value))}>
+          10%
+        </button>
+        <button value="15" onClick={(e) => onClick(Number(e.target.value))}>
+          15%
+        </button>
       </div>
       <div>
-        <button value="25">25%</button>
-        <button value="50">50%</button>
+        <button value="25" onClick={(e) => onClick(Number(e.target.value))}>
+          25%
+        </button>
+        <button value="50" onClick={(e) => onClick(Number(e.target.value))}>
+          50%
+        </button>
 
-        <input className="customButton" type="number" placeholder=" Custom" />
+        <input
+          className="customButton"
+          type="number"
+          placeholder=" Custom"
+          value={change}
+          onChange={(e) => onChangeBill(Number(e.target.value))}
+        />
       </div>
     </>
   );
@@ -66,21 +94,30 @@ function NumberOfPeople() {
     </div>
   );
 }
-function TipAmount() {
+function TipAmount({ tip, tip2 }) {
   return (
     <div className="tipAmount">
       <h3 className="headingThree">
-        Tip Amount <span className="amount1"> $0.00</span>
+        Tip Amount{" "}
+        <span className="amount1">
+          {" "}
+          ${tip}
+          {tip2}
+        </span>
       </h3>
       <p className="para">/ person</p>
     </div>
   );
 }
-function Total({ bill }) {
+function Total({ bill, tip, tip2 }) {
   return (
     <div className="totalAmount">
       <h3 className="headingThree">
-        Total <span className="amount2"> ${bill}</span>
+        Total{" "}
+        <span className="amount2">
+          {" "}
+          ${bill + tip} {bill + tip2}
+        </span>
       </h3>
       <p className="para">/ person</p>
     </div>
